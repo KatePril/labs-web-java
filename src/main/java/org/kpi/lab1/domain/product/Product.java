93122fa3430fe149a1c520765068d363fad769ea
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-09-24 11:38:14 +0300

    Add product domain

---
+++ src/main/java/org/kpi/lab1/domain/product/Product.java
@@ -0,0 +1,14 @@
+package org.kpi.lab1.domain.product;
+
+import lombok.Builder;
+import lombok.Value;
+import org.kpi.lab1.domain.category.Category;
+
+@Value
+@Builder(toBuilder = true)
+public class Product {
+    Long id;
+    String name;
+    Double price;
+    Category category;
+}
