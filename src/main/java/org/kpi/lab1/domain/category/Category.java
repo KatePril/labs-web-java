dd51a063aafea50dd5e317eb88dcca966fea33e3
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-09-24 11:37:21 +0300

    Add category domain

---
+++ src/main/java/org/kpi/lab1/domain/category/Category.java
@@ -0,0 +1,12 @@
+package org.kpi.lab1.domain.category;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder(toBuilder = true)
+public class Category {
+    Long id;
+    String name;
+    String description;
+}
