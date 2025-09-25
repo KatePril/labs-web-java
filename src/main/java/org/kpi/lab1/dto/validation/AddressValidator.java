04502104c742e325af7288fd0f5f7f5b0fd3a397
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-09-25 10:28:36 +0300

    Delete Email validator, add Address validator

--- src/main/java/org/kpi/lab1/dto/validation/AddressValidator.java
+++ src/main/java/org/kpi/lab1/dto/validation/AddressValidator.java
@@ -7,13 +7,14 @@ import org.springframework.stereotype.Component;
 import java.util.regex.Pattern;
 
 @Component
-public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
-    private static final String EMAIL_PATTERN = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+public class AddressValidator implements ConstraintValidator<ValidAddress, String> {
 
-    private static final Pattern EMAIL_PATTERN_PATTERN = Pattern.compile(EMAIL_PATTERN);
+    private static final String ADDRESS_PATTERN = "Street [A-Za-z]+, Building \\d+-\\d+[a-z]?$";
+
+    private static final Pattern ADDRESS_PATTERN_PATTERN = Pattern.compile(ADDRESS_PATTERN);
 
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        return EMAIL_PATTERN_PATTERN.matcher(s).matches();
+        return ADDRESS_PATTERN_PATTERN.matcher(s).matches();
     }
 }
---
+++ src/main/java/org/kpi/lab1/dto/validation/ValidAddress.java
@@ -0,0 +1,21 @@
+package org.kpi.lab1.dto.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE_USE, ElementType.PARAMETER, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = AddressValidator.class)
+@Documented
+public @interface ValidAddress {
+
+    String INVALID_ADDRESS_MSG = "The provided address is invalid. Valid address example: Street Green, Building 86-3";
+
+    String message() default INVALID_ADDRESS_MSG;
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
