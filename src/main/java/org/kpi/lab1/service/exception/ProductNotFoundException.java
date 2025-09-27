9b34f04d16aebad41b4192844e948a7b9dd6cbdf
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-09-27 18:28:41 +0300

    Add product not found exception

---
+++ src/main/java/org/kpi/lab1/service/exception/ProductNotFoundException.java
@@ -0,0 +1,9 @@
+package org.kpi.lab1.service.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+    private static final String EXCEPTION_MSG = "No product with id %s found";
+
+    public ProductNotFoundException(Long productId) {
+        super(String.format(EXCEPTION_MSG, productId));
+    }
+}
