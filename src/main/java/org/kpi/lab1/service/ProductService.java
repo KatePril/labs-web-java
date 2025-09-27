a3d92882130559ef4e7988f4054b1735ab2a7a0c
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-09-28 00:09:57 +0300

    Add product service

---
+++ src/main/java/org/kpi/lab1/service/ProductService.java
@@ -0,0 +1,18 @@
+package org.kpi.lab1.service;
+
+import org.kpi.lab1.domain.product.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getAllProducts();
+
+    Product getProductById(Long id);
+
+    Product addProduct(Product product);
+
+    Product updateProduct(Product product);
+
+    void deleteProduct(Long id);
+}
