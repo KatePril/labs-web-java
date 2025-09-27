8df8c4e420b43d9d219693c066eebf17604a941d
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-09-27 19:06:30 +0300

    Add dto for product list, add product mapper

---
+++ src/main/java/org/kpi/lab1/dto/product/ProductListDto.java
@@ -0,0 +1,18 @@
+package org.kpi.lab1.dto.product;
+
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+import java.util.List;
+
+@Value
+@Builder(toBuilder = true)
+@Jacksonized
+public class ProductListDto {
+
+    @NotNull(message = "Product list cannot be null")
+    List<ProductDto> products;
+}
---
+++ src/main/java/org/kpi/lab1/service/mapper/ProductMapper.java
@@ -0,0 +1,34 @@
+package org.kpi.lab1.service.mapper;
+
+import org.kpi.lab1.domain.category.Category;
+import org.kpi.lab1.domain.product.Product;
+import org.kpi.lab1.dto.category.CategoryDto;
+import org.kpi.lab1.dto.product.ProductDto;
+import org.kpi.lab1.dto.product.ProductListDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
+
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "description", source = "description")
+    @Mapping(target = "price", source = "price")
+    @Mapping(target = "category", source = "category", qualifiedByName = "toCategory")
+    ProductDto toProductDto(Product product);
+
+    default ProductListDto toProductListDto(List<Product> products) {
+        return ProductListDto.builder().products(toProductsDto(products)).build();
+    }
+
+    List<ProductDto> toProductsDto(List<Product> products);
+
+    @Named("toCategory")
+    default CategoryDto toCategoryDto(Category category) {
+        return null;
+    }
+}
