6d35cd11076cdd22cd2bba3c75276aaa1a1b776e
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-10-01 20:32:55 +0300

    Add cosmic word validator

--- src/main/java/org/kpi/lab1/dto/product/ProductDto.java
+++ src/main/java/org/kpi/lab1/dto/product/ProductDto.java
@@ -1,5 +1,6 @@
 package org.kpi.lab1.dto.product;
 
+import jakarta.validation.GroupSequence;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -7,16 +8,22 @@ import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
 import org.kpi.lab1.dto.category.CategoryDto;
+import org.kpi.lab1.dto.validation.ExtendedValidation;
+import org.kpi.lab1.dto.validation.ValidDescription;
+
+import java.security.cert.Extension;
 
 @Value
 @Builder(toBuilder = true)
 @Jacksonized
+@GroupSequence({ProductDto.class, Extension.class})
 public class ProductDto {
 
     @NotBlank(message = "Name is mandatory field")
     @Size(max = 90, message = "Name cannot exceed 90 characters")
     String name;
 
+    @ValidDescription(groups = ExtendedValidation.class)
     String description;
 
     @NotNull(message = "Price cannot be null")
---
+++ src/main/java/org/kpi/lab1/dto/validation/CosmicWordValidator.java
@@ -0,0 +1,19 @@
+package org.kpi.lab1.dto.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Pattern;
+
+
+@Component
+public class CosmicWordValidator implements ConstraintValidator<ValidDescription, String> {
+
+    private static final Pattern PATTERN = Pattern.compile("\\b(star|galaxy|comet)\\b", Pattern.CASE_INSENSITIVE);
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        return PATTERN.matcher(s).find();
+    }
+}
