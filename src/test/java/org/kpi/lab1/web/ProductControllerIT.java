9a01cb29268561e8e4325e7da98bb2ae6e6927e1
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-10-20 14:22:46 +0300

    Add header for ProductControllerIT

---
+++ src/test/java/org/kpi/lab1/web/ProductControllerIT.java
@@ -0,0 +1,10 @@
+package org.kpi.lab1.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+@AutoConfigureMockMvc
+@DisplayName("Product Controller IT")
+@Tag("product-service")
+public class ProductControllerIT {}
