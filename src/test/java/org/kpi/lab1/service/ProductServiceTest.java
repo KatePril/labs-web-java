9ba6869a31a35d94a4eac25013d389a5935adcbf
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-10-28 22:11:05 +0200

    Fix service level tests

--- src/test/java/org/kpi/lab1/service/ProductServiceTest.java
+++ src/test/java/org/kpi/lab1/service/ProductServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,6 +18,7 @@ import org.kpi.lab1.domain.product.Product;
 import org.kpi.lab1.service.implementation.ProductServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootTest(classes = {ProductServiceImplementation.class})
@@ -27,6 +29,9 @@ public class ProductServiceTest {
   private static final String PRODUCT_NAME = "product";
   private static final double PRODUCT_PRICE = 10.1;
   private static final Category CATEGORY = Category.builder().id(1L).name("test category").build();
+
+  @MockBean
+  private RateService rateService;
 
   @Autowired
   private ProductServiceImplementation productService;
@@ -61,6 +66,7 @@ public class ProductServiceTest {
   @Order(3)
   @DisplayName("Should add a new product")
   public void testAddProduct() {
+    when(rateService.getProductById(99L)).thenReturn(4.9);
     Product newProduct = buildProduct(99L);
 
     Product added = productService.addProduct(newProduct);
@@ -76,6 +82,7 @@ public class ProductServiceTest {
   @Order(4)
   @DisplayName("Should update existing product")
   public void testUpdateProduct() {
+    when(rateService.getProductById(99L)).thenReturn(4.9);
     Product updatedProduct =
         Product.builder().id(99L).name("Updated product").price(55.5).category(CATEGORY).build();
 
