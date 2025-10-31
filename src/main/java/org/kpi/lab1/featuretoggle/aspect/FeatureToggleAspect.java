8fe13a3b20e04decf2e7e460fab23c64f36ad85d
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-10-31 21:30:39 +0200

    Update Aspect, application.properties

--- src/main/java/org/kpi/lab1/featuretoggle/aspect/FeatureToggleAspect.java
+++ src/main/java/org/kpi/lab1/featuretoggle/aspect/FeatureToggleAspect.java
@@ -2,9 +2,11 @@ package org.kpi.lab1.featuretoggle.aspect;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.kpi.lab1.featuretoggle.FeatureToggleService;
 import org.kpi.lab1.featuretoggle.FeatureToggles;
 import org.kpi.lab1.featuretoggle.annotation.FeatureToggle;
@@ -18,17 +20,13 @@ import org.springframework.stereotype.Component;
 public class FeatureToggleAspect {
     private final FeatureToggleService featureToggleService;
 
-    @Around(value = "@annotation(featureToggle)")
-    public Object checkFeatureToggle(ProceedingJoinPoint joinPoint, FeatureToggle featureToggle) throws Throwable {
-        return checkToggle(joinPoint, featureToggle);
-    }
-
-    private Object checkToggle(ProceedingJoinPoint joinPoint, FeatureToggle featureToggle) throws Throwable {
+    @Before("@annotation(featureToggle)")
+    public void checkFeatureToggle(JoinPoint joinPoint, FeatureToggle featureToggle) {
         FeatureToggles toggle = featureToggle.value();
-        if (featureToggleService.checkFeatureToggle(toggle.getFeatureName())) {
-            return joinPoint.proceed();
+        if (!featureToggleService.checkFeatureToggle(toggle.getFeatureName())) {
+            log.warn("Feature toggle {} is disabled", toggle.getFeatureName());
+            throw new DisabledFeatureToggleException(toggle.getFeatureName());
         }
-        log.warn("Feature toggle {} is not enabled!", toggle.getFeatureName());
-        throw new DisabledFeatureToggleException(toggle.getFeatureName());
+        log.debug("Feature toggle {} is enabled", toggle.getFeatureName());
     }
 }
