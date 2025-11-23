689f0a56518bff5b3dbf39e24d66c0026b6359e8
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-11-23 14:29:35 +0200

    Fix Order repo

--- src/main/java/org/kpi/lab1/repository/OrderRepository.java
+++ src/main/java/org/kpi/lab1/repository/OrderRepository.java
@@ -8,5 +8,4 @@ import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
-    Optional<OrderEntity> findOrderByIdOrderByNameAsc(Long orderId);
 }
