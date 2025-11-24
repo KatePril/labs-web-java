4fca30cbb2a2708541c01a3ac7b369945effb220
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-11-24 11:21:38 +0200

    Add annotation to ProductServiceTest

--- src/test/java/org/kpi/lab1/service/ProductServiceTest.java
+++ src/test/java/org/kpi/lab1/service/ProductServiceTest.java
@@ -23,7 +23,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 
-@SpringBootTest
+@SpringBootTest(classes = {ProductServiceImplementation.class})
 @Import(MappersTestConfiguration.class)
 @DisplayName("Product Service Tests")
 @TestMethodOrder(OrderAnnotation.class)
